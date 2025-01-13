bc6d2dcf470faf7e9f8190c6f6d95ccbbef25907
Michael Leung <37495937+leungkimming@users.noreply.github.com>
2025-01-13 23:09:19 +0800

    Update Class2.cs

--- Class2.cs
+++ Class2.cs
@@ -3,7 +3,7 @@
     public class Test1
     {
         public string Password = "NMIfyYncKcRALEXAMPLE";
-        string AWS_ACCESS_KEY_ID = "AKIAJAA49FFSFRFN6AAA"
-        string AWS_SECRET_ACCESS_KEY = "u9N1o8s+u3q4uwt9s8dfsdf/afx/d/24449YiNHN"
+        string AWS_ACCESS_KEY_ID = "bcb53661c06b4728e59d897fb6165d5c9cda0fd9cdf9d09ead458168deb7518c"
+        string AWS_SECRET_ACCESS_KEY = "MEQCIQDaMKqrGnE27S0kgMrEK0eYBmyG0LeZismAEz/BgZyt7AIfXt9fErtRS4XaeSt/AO1RtBY66YcAdjxji410VQV4xg=="
     }
 }
