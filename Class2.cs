fc424d561e083311b81dc6c9971fda5a05d7a7a3
Michael Leung <37495937+leungkimming@users.noreply.github.com>
2025-01-16 19:00:22 +0800

    Update Class2.cs

--- Class2.cs
+++ Class2.cs
@@ -10,7 +10,7 @@ namespace WebApplication1
             using (SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Testing1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")) {
                 connection.Open();
                 string deleteCommandText = String.Format(@"DELETE FROM [dbo].[{0}] 
-                                                WHERE [{1}] < '{2}'", TableName, DateTimeColumn,
+                                                 WHERE [{1}] < '{2}'", TableName, DateTimeColumn,
                                                     DateTime.Now.AddDays(-5).ToString("yyyy-MM-dd HH:mm:ss"));
                 using (SqlCommand deleteCommand = new SqlCommand(deleteCommandText, connection)) {
                     deleteCommand.Parameters.AddWithValue("@RetentionDate", DateTime.Now.AddDays(-5).ToString("yyyy-MM-dd HH:mm:ss"));
@@ -27,7 +27,7 @@ namespace WebApplication1
             using (SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Testing1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")) {
                 connection.Open();
                 string deleteCommandText = String.Format(@"DELETE FROM [dbo].[{0}] 
-                                                WHERE [{1}] < '{2}'", TableName, DateTimeColumn,
+                                                 WHERE [{1}] < '{2}'", TableName, DateTimeColumn,
                                                     DateTime.Now.AddDays(-5).ToString("yyyy-MM-dd HH:mm:ss"));
                 using (SqlCommand deleteCommand = new SqlCommand(deleteCommandText, connection)) {
                     deleteCommand.Parameters.AddWithValue("@RetentionDate", DateTime.Now.AddDays(-5).ToString("yyyy-MM-dd HH:mm:ss"));
