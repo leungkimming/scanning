0a2b1cb8e7222f49e1b0502dcf7aa767dc0daeee
Michael Leung <37495937+leungkimming@users.noreply.github.com>
2025-01-16 14:50:32 +0800

    Update Class2.cs

--- Class2.cs
+++ Class2.cs
@@ -4,7 +4,7 @@ namespace WebApplication1
 {
     public class Test1
     {
-        public string Password = "ghp_pS8K3x72BQEE0S1fuuFgrqNptcXxYG1l8axC";
+        public string Password = "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";
         public void StartHandle(string TableName, string DateTimeColumn) {
             using (SqlConnection connection = new SqlConnection("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Testing1;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")) {
                 connection.Open();
